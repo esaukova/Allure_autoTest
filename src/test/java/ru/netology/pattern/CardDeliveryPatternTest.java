b523667dc1df63fac6cba6daafcecc8426e9874a
Ekaterina Saukova <esaukova@outlook.com>
2023-10-08 18:02:36 +0300

    Added allure configuration

--- src/test/java/ru/netology/pattern/CardDeliveryPatternTest.java
+++ src/test/java/ru/netology/pattern/CardDeliveryPatternTest.java
@@ -1,9 +1,10 @@
 package ru.netology.pattern;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+
 import java.time.Duration;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
@@ -17,6 +18,16 @@ public class CardDeliveryPatternTest {
     void setup() {
         open("http://localhost:9999");
     }
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
 
     @Test
 
